a04fce85ce3869b40b6f15aef5ecfafe6985175f
daniel96a <daniel.allbrand@hotmail.com>
2020-01-09 15:36:31 +0100

    added reports to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,14 @@ import { combineReducers } from "redux";
 import auth from "./auth";
 import employees from "./employees";
 import customers from "./customers";
+import reports from "./reports";
+
 
 const rootReducer = combineReducers({
   auth,
   customers,
-  employees
+  employees,
+  reports,
 });
 
 export default rootReducer;
