9a785a642b2449f76923a08560fefcf53daf2457
daniel96a <daniel.allbrand@hotmail.com>
2020-02-20 19:10:39 +0100

    n/a

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -2,7 +2,7 @@ import * as types from "./types";
 import axios from "axios";
 import { customHeaders } from "./customHeaders";
 import history from "../../history";
-import base64 from "base-64";
+// import base64 from "base-64";
 import { setGrantTypePassword } from "../../utils/setGrantType";
 import { setAuthorizationToken } from "../../utils/setAuthorizationToken";
 
@@ -29,26 +29,18 @@ export const verifyToken = token => {
 };
 
 export const doLogin = data => {
-  var encodedData =
-    "Basic " + base64.encode(`${data.username}:${data.password}`);
-  console.log(base64.decode("cGVyaGFtOjEyMzQ="));
-  console.log(encodedData);
+  // var encodedData =
+  //   "Basic " + base64.encode(`${data.username}:${data.password}`);
   return async dispatch => {
     axios
       .post("/oauth/token", setGrantTypePassword(data), {
         headers: {
-          // "Content-Type": "application/x-www-form-urlencoded",
-          customHeaders,
-          // refresh_token: `bearer ${axios.defaults.headers.common["access_token"]}`,
-          
           Authorization: "Basic cGVyaGFtOjEyMzQ="
         },
         timeout: 1000
       })
       .then(token => {
-        console.log(token);
         setAuthorizationToken(token);
-        // sessionStorage.setItem("loggedin", res.data);
         dispatch(setCurrentUser(token));
       })
       .catch(error => {
