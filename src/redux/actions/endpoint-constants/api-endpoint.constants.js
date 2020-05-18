57b6c404a4867a079061490ab94ec43bde56c041
Daniel Allbrand <daniel.allbrand@hotmail.com>
2020-05-18 20:08:56 +0200

    better responsivness

---
+++ src/redux/actions/endpoint-constants/api-endpoint.constants.js
@@ -0,0 +1,21 @@
+export const API_ENDPOINTS = {
+  company: {
+    all: "/company",
+  },
+  person: {
+    all: "/person",
+    one: "/person/:id",
+  },
+  customer: {
+    all: "/customer",
+    one: "/customer/:id",
+  },
+  employee: {
+    all: "/employee",
+    one: "/employee/:id",
+  },
+  report: {
+    all: "/report",
+    one: "/report/:id",
+  },
+};
---
+++ src/utils/authorizationParams.js
@@ -0,0 +1,5 @@
+import Cookies from "universal-cookie";
+const cookie = new Cookies();
+const access_token = cookie.get("access_token");
+
+export const authorizationParams = `/?access_token=${access_token}`;
